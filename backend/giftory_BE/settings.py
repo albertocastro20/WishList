b91a74872f4529b49c66e577ce4b42619a230e42
Cristian Alberto Castro López <albertocastro05206@gmail.com>
2026-02-05 17:39:20 -0600

    Corregido el error del STATIC_ROOT

--- backend/giftory_BE/settings.py
+++ backend/giftory_BE/settings.py
@@ -141,13 +141,15 @@ STATIC_URL = '/static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://wish-list-rose.vercel.app/",
+    "https://wish-list-rose.vercel.app",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching

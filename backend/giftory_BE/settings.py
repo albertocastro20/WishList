1ac136f1e6a382fc07ef596ec4f2be3c94d64fee
Cristian Alberto Castro López <albertocastro05206@gmail.com>
2026-02-05 17:58:27 -0600

    Corregido lo de los HOSTS

--- backend/giftory_BE/settings.py
+++ backend/giftory_BE/settings.py
@@ -25,9 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pu!es%736qj8-#w5-vkx2=uc5!8&(nso^scjjxwj#sgfhwml#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
+
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
